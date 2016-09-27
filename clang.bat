e3cf5b334100f55779d59eb1809afa47968e733f
Pierre LeMoine <git@luben.se>
2016-09-27 18:20:37 +0200

    Added two overloads of takeWhile
    One with a parameter bool(T) and one with bool(T,size_t)
    Implemented it in a silly way because I was tired.

--- clang.bat
+++ clang.bat
@@ -2,5 +2,5 @@ set CC=clang
 set CXX=clang++
 mkdir build-clang
 cd build-clang
-cmake .. -G "Unix Makefiles" && cmake --build . && ctest ..
+cmake .. -G "Unix Makefiles" && cmake --build . && ctest .. -V
 cd ..
--- test/test_take.cpp
+++ test/test_take.cpp
@@ -33,3 +33,46 @@ TEST(Take, Everything)
 	int array[3] = { 1,2,3 };	
 	EXPECT_EQ(3, Enumerable(array).take(20).count());
 }
+
+TEST(TakeWhile, Everything)
+{
+	int array[3] = { 1,2,3 };	
+	EXPECT_EQ(3, Enumerable(array).take([](auto x){ return true;}).count());
+}
+
+TEST(TakeWhile, Nothing)
+{
+	int array[3] = { 1,2,3 };	
+	EXPECT_EQ(0, Enumerable(array).take([](auto x){ return false;}).count());
+}
+
+TEST(TakeWhile, Some)
+{
+	int array[3] = { 1,2,3 };	
+	EXPECT_EQ(2, Enumerable(array).take([](auto x){ return x < 3;}).count());
+}
+
+TEST(TakeWhileIdx, Take1)
+{
+	int array[3] = { 1,2,3 };	
+	int count;
+	EXPECT_EQ(1, Enumerable(array).take([&](auto x, auto idx){ count = idx; return idx < 1;}).count());
+	EXPECT_EQ(1, count);
+}
+
+TEST(TakeWhileIdx, Take2)
+{
+	int array[3] = { 1,2,3 };	
+	int count;
+	EXPECT_EQ(2, Enumerable(array).take([&](auto x, auto idx){ count = idx; return idx < 2;}).count());
+	EXPECT_EQ(2, count);
+}
+
+TEST(TakeWhileIdx, Take3)
+{
+	int array[3] = { 1,2,3 };
+	int count;
+	EXPECT_EQ(3, Enumerable(array).take([&](auto x, auto idx){ count = idx; return idx < 3;}).count());
+	EXPECT_EQ(2, count);
+}
+
